dddc187c4b45d9efb5732e0a5bf49d81a1fb4178
Sahil-0912 <https://github.com/Sahil-0912/rnw.git>
2024-12-18 11:02:34 +0530

    add files 2

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import About from './About'
-import AboutContent from './AboutCOntent'
 import OurMissionVission from './OurMissionVission'
+import AboutContent from './AboutContent'
 
 const AboutUs = () => {
   return (
